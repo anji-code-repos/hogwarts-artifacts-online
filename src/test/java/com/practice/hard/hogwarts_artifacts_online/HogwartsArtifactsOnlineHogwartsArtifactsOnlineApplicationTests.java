cc1acea5519fb657ed7705541cd93922b2b5ca8a
anji <anjibabuvanka@gmail.com>
2025-06-04 12:50:48 +0530

    This is the first commit

---
+++ src/test/java/com/practice/hard/hogwarts_artifacts_online/HogwartsArtifactsOnlineHogwartsArtifactsOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.hard.hogwarts_artifacts_online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsArtifactsOnlineHogwartsArtifactsOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
